145d4ebf2b9de0b9578a77485f19742795bcfea9
siddapareddy <vishnu.040890@gmail.com>
2025-03-09 14:41:35 -0400

    second COMMIT

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const parent = React.createElement("div",{id:"parent"},
     ),
       React.createElement("div",{id:"child2"},
       [React.createElement("h1",{},"I am h1 Tag"),
-      React.createElement("h2",{},"I am h2 Tag")]
+      React.createElement("h2",{},"I am h2 Tag s")]
     )
 ])
 
